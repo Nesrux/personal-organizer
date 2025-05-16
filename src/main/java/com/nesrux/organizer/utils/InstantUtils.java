2d885c280c5598886d8279e6102a044ad04dc0c0
Nesrux <joaomarcosdevs@gmail.com>
2025-05-16 18:20:40 -0300

    feat: create a new utils

---
+++ src/main/java/com/nesrux/organizer/utils/InstantUtils.java
@@ -0,0 +1,15 @@
+package com.nesrux.organizer.utils;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+public final class InstantUtils {
+
+    private InstantUtils() {
+    }
+
+    public static Instant now() {
+        return Instant.now().truncatedTo(ChronoUnit.MICROS);
+    }
+
+}
