7ca357c321459d1b4be853a881c087c1392f1b65
Nesrux <joaomarcosdevs@gmail.com>
2025-05-19 13:27:17 -0300

    chore: configure openApi documentation

---
+++ src/main/java/com/nesrux/organizer/configuration/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package com.nesrux.organizer.configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "organizer API",
+                version = "v1",
+                description = "API for task management",
+                license = @License(
+                        name = "MIT",
+                        url = "colocar depois"
+                ),
+                contact = @Contact(
+                        name = "João marcos",
+                        email = "joaomarcosdevs@gmail.com",
+                        url = "https://www.linkedin.com/in/joaomarcosdev/"
+                )
+        )
+)
+public class OpenApiConfig {
+}
