21a77ed4ee39b9cbdf07b4678dc48b525b7dd5c5
Nesrux <joaomarcosdevs@gmail.com>
2025-05-19 14:00:28 -0300

    feat: crate a documentation from TaskApi

--- src/main/java/com/nesrux/organizer/infrastructure/api/controllers/TaskController.java
+++ src/main/java/com/nesrux/organizer/infrastructure/api/controllers/TaskController.java
@@ -1,24 +1,25 @@
 package com.nesrux.organizer.infrastructure.api.controllers;
 
-import java.util.List;
-
 import com.nesrux.organizer.domain.models.category.CategoryGateway;
 import com.nesrux.organizer.domain.models.task.Frequency;
 import com.nesrux.organizer.domain.models.task.Task;
+import com.nesrux.organizer.domain.models.task.TaskGateway;
+import com.nesrux.organizer.infrastructure.api.docs.TaskApi;
+import com.nesrux.organizer.infrastructure.api.models.task.TaskInputDto;
+import com.nesrux.organizer.infrastructure.api.models.task.TaskInputId;
+import com.nesrux.organizer.infrastructure.api.models.task.TaskListDTO;
+import com.nesrux.organizer.infrastructure.api.models.task.TaskOutputDto;
 import jakarta.transaction.Transactional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import com.nesrux.organizer.domain.models.task.TaskGateway;
-import com.nesrux.organizer.infrastructure.api.models.task.TaskInputDto;
-import com.nesrux.organizer.infrastructure.api.models.task.TaskListDTO;
-import com.nesrux.organizer.infrastructure.api.models.task.TaskOutputDto;
+import java.util.List;
 
 @RestController
 @RequestMapping("/tasks")
 @CrossOrigin("*")
-public class TaskController {
+public class TaskController implements TaskApi {
 
     private final TaskGateway service;
     private final CategoryGateway categoryGateway;
@@ -28,27 +29,25 @@ public class TaskController {
         this.categoryGateway = categoryGateway;
     }
 
+    @Override
     @GetMapping("/{id}")
-    public ResponseEntity<TaskOutputDto> findById(@PathVariable String id) {
-        var task = service.findTaskById(id);
+    public ResponseEntity<TaskOutputDto> findById(final TaskInputId inputId) {
+        var task = service.findTaskById(inputId.id());
         return ResponseEntity.ok(TaskOutputDto.with(task));
     }
 
+    @Override
     @GetMapping
     public List<TaskListDTO> findAll() {
         var tasks = service.listTasks();
         return tasks.stream().map(TaskListDTO::fromDomain).toList();
     }
 
+    @Override
     @PostMapping
     @Transactional
-    public ResponseEntity<TaskOutputDto> save(@RequestBody TaskInputDto taskDto) {
-        System.out.println(taskDto.categoryId());
-        System.out.println(taskDto.categoryId());
-        System.out.println(taskDto.categoryId());
-
+    public ResponseEntity<TaskOutputDto> save(final TaskInputDto taskDto) {
         var category = categoryGateway.getCategoryById(taskDto.categoryId());
-
 
         var task = Task.create(
                 taskDto.title(),
@@ -61,9 +60,10 @@ public class TaskController {
         return ResponseEntity.status(HttpStatus.CREATED).body(TaskOutputDto.with(saved));
     }
 
+    @Override
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable String id) {
-        service.deleteTaskById(id);
+    public ResponseEntity<Void> delete(final TaskInputId inputId) {
+        service.deleteTaskById(inputId.id());
         return ResponseEntity.noContent().build();
     }
 }
---
+++ src/main/java/com/nesrux/organizer/infrastructure/api/models/task/TaskInputId.java
@@ -0,0 +1,4 @@
+package com.nesrux.organizer.infrastructure.api.models.task;
+
+public record TaskInputId( String id) {
+}
